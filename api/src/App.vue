ee61fb49edf00a040122f724ffa52ab4861b1b8a
Vinicius Cruz <vinaocruz@gmail.com>
2018-05-15 00:24:55 -0300

    Componente gerenciar produtos

--- api/src/App.vue
+++ api/src/App.vue
@@ -1,25 +1,14 @@
 <template>
-   <product-list></product-list>
+   <manage-products></manage-products>
 </template>
 
 <script>
-import ProductList from './components/ProductList'
+import ManageProducts from './components/ManageProducts'
 
 export default {
   name: 'App',
   components: {
-    ProductList
+    ManageProducts
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
