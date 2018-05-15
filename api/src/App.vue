1cd625c22412afc65451261246639fd863d87dcb
Vinicius Cruz <vinaocruz@gmail.com>
2018-05-15 00:14:26 -0300

    Loja

---
+++ api/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+   <product-list></product-list>
+</template>
+
+<script>
+import ProductList from './components/ProductList'
+
+export default {
+  name: 'App',
+  components: {
+    ProductList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
