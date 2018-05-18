a919a508b81f1316e19b2723494f63b0062f0710
Vinicius Cruz <vinaocruz@gmail.com>
2018-05-17 23:08:10 -0300

    Seleciona produto pra edita-lo

--- app/src/components/ManageProducts.vue
+++ app/src/components/ManageProducts.vue
@@ -1,64 +1,78 @@
 <template>
-	<section>
-		<save-product-form :product="productInForm" v-on:submit="onFormSave">
-		</save-product-form>
-		<product-list :products='products'></product-list>
-	</section>
+    <section>
+        <save-product-form :product="productInForm" v-on:submit="onFormSave">
+        </save-product-form>
+        <product-list :products='products' v-on:edit="onEditClicked">
+        </product-list>
+    </section>
 </template>
 
 <script>
-	import ProductList from './ProductList'
-	import SaveProductForm from './SaveProductForm'
-	import uuid from 'uuid'
+    import ProductList from './ProductList'
+    import SaveProductForm from './SaveProductForm'
+    import uuid from 'uuid'
 
-	const initialData = () => {
-		return {
-			productInForm: {
-				id: null,
-				name: '',
-				description: '',
-				price: null
-			},
-			products: [
-				{
-		          id: 'cc919e21-ae5b-5e1f-d023-c40ee669520c',
-		          name: 'COBOL 101 vintage',
-		          description: 'Learn COBOL with this vintage programming book',
-		          price: 399
-		        },
-		        {
-		          id: 'bcd755a6-9a19-94e1-0a5d-426c0303454f',
-		          name: 'Sharp C2719 curved TV',
-		          description: 'Watch TV like never before with the brand new curved screen technology',
-		          price: 1995
-		        },
-		        {
-		          id: '727026b7-7f2f-c5a0-ace9-cc227e686b8e',
-		          name: 'Remmington X mechanical keyboard',
-		          description: 'Excellent for gaming and typing, this Remmington X keyboard ' +
-		            'features tactile, clicky switches for speed and accuracy',
-		          price: 595
-		        }				
-			]
-		}
-	}
+    const initialData = () => {
+        return {
+            productInForm: {
+                id: null,
+                name: '',
+                description: '',
+                price: null
+            },
+            products: [
+                {
+                  id: 'cc919e21-ae5b-5e1f-d023-c40ee669520c',
+                  name: 'COBOL 101 vintage',
+                  description: 'Learn COBOL with this vintage programming book',
+                  price: 399
+                },
+                {
+                  id: 'bcd755a6-9a19-94e1-0a5d-426c0303454f',
+                  name: 'Sharp C2719 curved TV',
+                  description: 'Watch TV like never before with the brand new curved screen technology',
+                  price: 1995
+                },
+                {
+                  id: '727026b7-7f2f-c5a0-ace9-cc227e686b8e',
+                  name: 'Remmington X mechanical keyboard',
+                  description: 'Excellent for gaming and typing, this Remmington X keyboard ' +
+                    'features tactile, clicky switches for speed and accuracy',
+                  price: 595
+                }                
+            ]
+        }
+    }
 
-	export default {
-		components: {
-			ProductList,
-			SaveProductForm
-		},
-		data: initialData,
-		methods: {
-			onFormSave(product) {
-				// console.log('productData', JSON.stringify(product))
-				product.id = uuid.v4()
-				this.products.push(product)
-				this.resetProductForm()
-			},
-			resetProductForm() {
-				this.productInForm = initialData().productInForm
-			}
-		}
-	}
+    export default {
+        components: {
+            ProductList,
+            SaveProductForm
+        },
+        data: initialData,
+        methods: {
+            onFormSave(product) {
+	          // console.log('productData', JSON.stringify(product))
+			  const index = this.products.findIndex((p) => p.id === product.id)
+
+		      // update product if it exists or create it if it doesn't
+		      if (index !== -1) {
+		        // We need to replace the array entirely so that vue can recognize
+		        // the change and re-render entirely.
+		        // See http://vuejs.org/guide/list.html#Caveats
+		        this.products.splice(index, 1, product)
+		      } else {
+		        product.id = uuid.v4()
+		        this.products.push(product)
+		      }
+              this.resetProductForm()
+            },
+            resetProductForm() {
+                this.productInForm = initialData().productInForm
+            },
+            onEditClicked (product) {
+                this.productInForm = { ...product };
+            }
+        }
+    }
 </script>
